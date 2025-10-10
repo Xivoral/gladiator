3c120f23b20392123a42a1edad2c201359834350
Tor Blom <tor.blom@learnet.se>
2025-10-10 13:24:29 +0200

    fungerar

--- Gladiatorerna.py
+++ Gladiatorerna.py
@@ -14,32 +14,71 @@ class Gladiator:
         return self.hälsa
     
     def visa_styrka(self):
-        return self.hälsa   
+        return self.styrka   
+
+    def ta_skada(self, skada):
+        self.hälsa -= skada
+
     
     def visa_snabbhet(self):
         return self.hälsa
-    
-Flamma = Gladiator("Flamma", 4, 8, 8)
-Titus = Gladiator("Titus", 12, 3, 1)
-Gaius = Gladiator("Gaius", 1, 20, 3, 100)
-Gelidus = Gladiator("Gelidus", 20, 1, 1)
-General = Gladiator("General", 100, 10, 1)
-Fiende = Gladiator("Fiende", 100, 10, 1)
-
-
-print(Flamma.visa_hälsa())
+Gladiator1 = Gladiator("Kämpe", 100, 10, 10)
+Gladiator2 = Gladiator("Fiende", 100, 10, 10)
 
 strid = True
 
 while strid == True:
-    attack = int(input("(1)Slå (2)Sparka (3)Kasta: "))
-    if attack == 1:
-        Fiende.hälsa -= General.styrka * random.randint(1, 1.5)
+    attack = input("(1)Slå (2)Sparka (3)Kasta: ")
+    fiende_attack = str(random.randint(1,3))
+
+
+    if attack == "1":
+        Gladiator2.ta_skada(Gladiator1.styrka)
+        print(f"Du attackerar fienden med ett slag och den tar " + str(Gladiator1.visa_styrka()) + " skada ")
+        print(f"Fienden har nu " + str(Gladiator2.visa_hälsa()) + " kvar.")
+        print(Gladiator2.visa_hälsa())
+
+    elif attack == "2":
+        Gladiator2.ta_skada(Gladiator1.styrka)
+        print(f"Du attackerar fienden med en spark och den tar " + str(Gladiator1.visa_styrka()) + " skada ")
+        print(f"Fienden har nu " + str(Gladiator2.visa_hälsa()) + " kvar.")
+        print(Gladiator2.visa_hälsa())
     
-    print(Fiende.visa_hälsa)
+    elif attack == "3":
+        Gladiator2.ta_skada(Gladiator1.styrka)
+        print(f"Du attackerar fienden med ett kast och den tar " + str(Gladiator1.visa_styrka()) + " skada ")
+        print(f"Fienden har nu " + str(Gladiator2.visa_hälsa()) + " hälsa kvar.")
+        print(Gladiator2.visa_hälsa())
+    else:
+        print("Du skrev fel, gå vidare till nästa runda")
+        continue
+    
+    if fiende_attack == "1":
+        Gladiator1.ta_skada(Gladiator2.styrka)
+        print(f"Fienden attackerar dig med ett slag och du tar " + str(Gladiator2.visa_styrka()) + " skada ")
+        print(f"Fienden har nu " + str(Gladiator1.visa_hälsa()) + " kvar.")
+        print(Gladiator1.visa_hälsa())
 
-strid = False
+    elif fiende_attack == "2":
+        Gladiator1.ta_skada(Gladiator2.styrka)
+        print(f"Fienden attackerar dig med en spark och du tar " + str(Gladiator1.visa_styrka()) + " skada ")
+        print(f"Du har nu " + str(Gladiator1.visa_hälsa()) + " kvar.")
+        print(Gladiator1.visa_hälsa())
+    
+    elif fiende_attack == "3":
+        Gladiator1.ta_skada(Gladiator2.styrka)
+        print(f"Fienden attackerar dig med ett kast och du tar " + str(Gladiator1.visa_styrka()) + " skada ")
+        print(f"Du har nu " + str(Gladiator1.visa_hälsa()) + " hälsa kvar.")
+        print(Gladiator1.visa_hälsa())
 
+
+
+    if Gladiator2.visa_hälsa() <= 0:
+        print("Du vann!!")
+        strid = False
+    elif Gladiator1.visa_hälsa() <= 0:
+        print("Du förlorade!!")
+        strid = False
     
 
 
